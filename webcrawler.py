e9c51756710b270da535faf14e93acb36c996bcc
JamesGuenther <lordcheetah@comcast.net>
2017-03-17 22:08:01 -0400

    feat: Added ranks graph

--- webcrawler.py
+++ webcrawler.py
@@ -48,19 +48,42 @@ def add_page_to_index(index, url, content):
     for word in words:
         add_to_index(index, word, url)
 
+def compute_ranks(graph):
+    d = 0.8 # damping factor
+    numloops = 10
+
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for node in graph:
+                if page in graph[node]:
+                    newrank += d * (ranks[node] / len(graph[node]))
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
+    
 def crawl_web(seed):
     tocrawl = [seed]
     crawled = []
     index = {}
+    graph = {}
     while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
             content = get_page(page)
             add_page_to_index(index, page, content)
-            union(tocrawl, get_all_links(content))
+            outlinks = get_all_links(content)
+            graph[page] = outlinks
+            union(tocrawl, outlinks)
             crawled.append(page)
-    return index
+    return index, graph
 
 seed = "http://xkcd.com/353"
-print (crawl_web(seed))
-
+index, graph = crawl_web(seed)
+print graph
